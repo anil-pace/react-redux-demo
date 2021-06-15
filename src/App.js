7243449c9b023a74c37b96437b817af85048d22d
anil <anil.pace7@gmail.com>
2021-06-15 19:42:19 +0530

    Using localStorage to Save Data Across Windows

--- src/App.js
+++ src/App.js
@@ -7,28 +7,39 @@ import Dashboard from  './components/Dashboard';
 import Login from './components/Login/Login';
 import Preferences from './components/Preferences';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import useToken from './useToken';
 
 
-function setToken(userToken){
-  console.log("============> setToken function is called");
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
+// function setToken(userToken){
+//   console.log("============> setToken function is called");
+//   sessionStorage.setItem('token', JSON.stringify(userToken));
+//   console.log("============>token is" + JSON.stringify(userToken));
 
-function getToken(){
-  console.log("============> getToken function is called");
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
+// }
+
+// function getToken(){
+//   console.log("============> getToken function is called");
+//   const tokenString = sessionStorage.getItem('token');
+//   const userToken = JSON.parse(tokenString);
+//   return userToken?.token
+// }
 
 
 function App() {
 
-  const token = getToken();
+  // const token = getToken();
   // const [token, setToken] = useState();
+  const {token, setToken} = useToken();
+
+  console.log("========================> token is: "  + token);
 
   if(!token) {
     return <Login setToken={setToken} />
+  }
+
+  const logout =  e => {
+    sessionStorage.clear();
+    console.log("anil ============token cleared", sessionStorage.getItem('token'));
   }
 
   return (
@@ -36,6 +47,8 @@ function App() {
       <div className="wrapper">
 
         <h1>All React Redux Node demo, POC, homeworks here</h1>
+
+        <button onClick={logout}>Logout</button>
         
         <BrowserRouter>
           <Switch>
