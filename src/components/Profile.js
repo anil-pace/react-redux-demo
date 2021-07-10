9848ee818f402981123dd97eaab6364615925e87
anil <anil.pace7@gmail.com>
2021-07-10 21:07:12 +0530

    Async basic call using fetch

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,4 @@
+import { Update } from '@material-ui/icons';
 import React from 'react'
 import {useSelector, useDispatch} from "react-redux";
 // import {updateStatus} from "./../redux/actions/userAction"; // action creator way
@@ -17,8 +18,11 @@ export default function Profile() {
         // dispatch({type:"UPDATE_AGE", payload: age})
 
     }
-    const changeName=(name)=>{
-        dispatch(updateName(name));
+    const changeName= async (name)=>{
+        const res = await fetch("https://jsonplaceholder.typicode.com/users")
+        const res2 = await res.json();
+        dispatch(updateName(res2[0].name));
+        // dispatch(updateName(name));
         // dispatch({type:"UPDATE_NAME", payload: name})
     }
     const ChangeStatus=(status)=>{
